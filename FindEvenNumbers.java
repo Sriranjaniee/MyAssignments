f6ec23c690c4a65bdb40a85bd05eb24e7c0cd5ad
Sriranjaniee <165486383+Sriranjaniee@users.noreply.github.com>
2024-04-01 15:49:54 +0530

    Add files via upload

---
+++ FindEvenNumbers.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class FindEvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int maxRange= 10;
+		for (int a=0;a<=maxRange;a++)
+		{
+			if(a%2==0)
+			{
+				System.out.println("Even number is :" +a);
+			}
+		}
+	}
+
+}
---
+++ LearnIfElse.java
@@ -0,0 +1,26 @@
+package practice;
+
+public class LearnIfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int maxRange= 20;
+		for (int a=10;a<=maxRange;a++)
+		{
+			if(a%2==0)
+			{
+				System.out.println("even number is :" +a);
+			}
+			else
+			{
+				System.out.println("odd number is :" +a);
+				
+			}
+	}
+		}
+}
+
+	
+		
+
+
