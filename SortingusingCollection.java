fd0f14f6487301164b4955b524f70833878fabac
Sriranjaniee <165486383+Sriranjaniee@users.noreply.github.com>
2024-04-16 22:34:22 +0530

    Add files via upload

---
+++ SortingusingCollection.java
@@ -0,0 +1,24 @@
+package week3.day1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortingusingCollection {
+
+	public static void main(String[] args) {
+		
+		List<String> mentors=new ArrayList<String>();
+		mentors.add(",AspireSystems");
+		mentors.add(",CTS");
+		mentors.add(",HCL");
+		mentors.add("Wipro");
+		
+		Collections.reverse(mentors);
+		for(int i=0;i<mentors.size();i++)
+		{
+			System.out.print(mentors.get(i));
+		}
+	}
+
+}
